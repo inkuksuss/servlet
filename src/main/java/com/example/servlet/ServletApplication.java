6ff082cbcdca87bc02cc89c5614faf505df45ee6
heart <heart@famppy.com>
2023-02-03 00:40:27 +0900

    spring mvc

--- src/main/java/com/example/servlet/ServletApplication.java
+++ src/main/java/com/example/servlet/ServletApplication.java
@@ -3,6 +3,8 @@ package com.example.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
 
 @ServletComponentScan
 @SpringBootApplication
